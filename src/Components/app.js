85f19adcab5c8c4745a15c64e18b080d28e73d08
ekanshiag <ekanshi001@gmail.com>
2018-10-13 19:18:31 +0530

    Show boards at homepage

--- src/Components/app.js
+++ src/Components/app.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react'
 import Header from './header'
 import Boards from './boards'
 import Board from './board'
-import {BrowserRouter as Router, Route, Redirect} from 'react-router-dom'
+import {BrowserRouter as Router, Route} from 'react-router-dom'
 
 class App extends Component {
   render () {
@@ -10,8 +10,7 @@ class App extends Component {
       <Router>
         <div>
           <Route path='/' render={props => <Header />} />
-          <Route exact path='/' render={() => (<Redirect to='/boards' />)} />
-          <Route exact path='/boards' render={props => <Boards />} />
+          <Route exact path='/' render={props => <Boards />} />
           <Route exact path='/b/:boardId' render={props => <Board {...props} />} />
         </div>
       </Router>
